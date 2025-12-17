86fa576483e53c16d727c2af78ff942c22884e5c
VANJOO322 <i.bozhichkov2021@pmg-pernik.com>
2025-12-17 13:41:28 +0200

    test6

---
+++ WebAppShop_test/WebAppShop_test/Models/Client/ClientIndexVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppShop_test.Models.Client
+{
+    public class ClientIndexVM
+    {
+        
+            public string Id { get; set; } = null!;
+
+            [Display(Name = "Username")]
+            public string UserName { get; set; } = null!;
+
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; } = null!;
+
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; } = null!;
+
+            public string Address { get; set; } = null!;
+
+            public string Email { get; set; } = null!;
+
+            public bool IsAdmin { get; set; }
+        
+    }
+}
